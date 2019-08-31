a105e9b5f631465b858a3d485ee6b77eae96d7dd
Stanislav Samboretskiy <sword92@gmail.com>
2019-08-31 11:50:39 +0300

    Add simple direct compression

--- GZipTest/GZipParallel.cs
+++ GZipTest/GZipParallel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.IO.Compression;
 
 namespace GZipTest
@@ -10,18 +11,55 @@ namespace GZipTest
 
     public static class GZipParallel
     {
-        public static GZipParallel()
+
+        private static string directoryPath = @".\temp";
+        static GZipParallel()
         {
         }
 
-        public static void Compress()
+        public static void Compress(DirectoryInfo directorySelected)
         {
+            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+            {
+                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                {
+                    if ((File.GetAttributes(fileToCompress.FullName) &
+                       FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                    {
+                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                        {
+                            using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                               CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
 
+                            }
+                        }
+                        FileInfo info = new FileInfo(directoryPath + Path.DirectorySeparatorChar + fileToCompress.Name + ".gz");
+                        Console.WriteLine($"Compressed {fileToCompress.Name} from {fileToCompress.Length.ToString()} to {info.Length.ToString()} bytes.");
+                    }
+
+                }
+            }
         }
 
-        public static void Decompress()
-        {
 
+        public static void Decompress(FileInfo fileToDecompress)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Console.WriteLine($"Decompressed: {fileToDecompress.Name}");
+                    }
+                }
+            }
         }
     }
 }
--- GZipTest/Program.cs
+++ GZipTest/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
 
 namespace GZipTest
 {
     class Program
     {
+        private static string directoryPath = @".\temp";
         static void Help()
         {
             Console.WriteLine("How to use this program?");
@@ -17,16 +20,19 @@ namespace GZipTest
         static int Main(string[] args)
         {
             // Input args: compress/decompress [inputFileName] [outputFileName]
-            if (args.Length == 0)
+            //if (args.Length == 0)
+            //{
+              //  Help();
+                //return 1;
+            //}
+            DirectoryInfo directorySelected = new DirectoryInfo(directoryPath);
+            GZipParallel.Compress(directorySelected);
+
+            foreach (FileInfo fileToDecompress in directorySelected.GetFiles("*.gz"))
             {
-                Help();
-                return 1;
-            }
-
-            Console.WriteLine(args[0], args[1], args[2]);
-
+                GZipParallel.Decompress(fileToDecompress);
+            }        
             
-
             return 0;
         }
     }
